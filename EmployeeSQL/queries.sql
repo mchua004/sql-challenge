01630e24de02ee11ef21f1d0e0970e20d21ae4fb
unknown <mchua004@gmail.com>
2021-02-27 09:51:53 -0500

    Finished quereies

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -89,4 +89,12 @@ ON
 	de.dept_no = d.dept_no
 WHERE
 	d.dept_name = 'Sales' OR d.dept_name = 'Development'
-
+	
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
+SELECT DISTINCT
+	e.last_name,
+	COUNT(e.last_name) last_name_count
+FROM employees e
+GROUP BY
+	1
+ORDER BY 1 DESC
